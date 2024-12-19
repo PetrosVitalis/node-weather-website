e9becbb40bbe52655fbb32226acfc6f99251bbc5
PetrosVitalis <pvitalis1118@gmail.com>
2024-12-19 18:18:44 +0200

    Init commit

---
+++ public/js/app.js
@@ -0,0 +1,12 @@
+console.log('Client side js file loaded')
+
+fetch('http://localhost:3000/weather?address=%22boston%22').then((response) => {
+    response.json().then((data) => {
+        if (data.error) {
+            return console.log(data.error)
+        }
+        console.log(data.longitude) 
+        console.log(data.latitude)
+        console.log(data.fdata)
+    })
+})
---
+++ src/utils/forecast.js
@@ -0,0 +1,20 @@
+const request = require('request')
+const log = console.log
+
+const forecast = (longitude, latitude, callback) => {
+    const url = 'https://api.weatherstack.com/current?access_key=a74fd090ba4adc0476398af3263f29b3&query=' + latitude + ',' + longitude + ''
+    request( { url, json: true }, (error, {body}) => {
+        if (error) {
+            callback('Unable to connect', undefined)
+        } else if(body.error) {
+            callback('Cant find location', undefined)  
+        } else {
+            callback(undefined, {
+                Temp: body.current.temperature,
+                feels_like: body.current.feelslike
+            })
+        }
+    })
+}
+
+module.exports = forecast
